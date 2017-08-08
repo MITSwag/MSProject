d284f2ff8e688a0e0e8f99e137aaf91765ec47fa
mchen2525 <michen2525@icloud.com>
2017-08-08 11:47:11 -0500

    Update and rename script.js to firebaseScript.js

---
+++ firebaseScript.js
@@ -0,0 +1,9 @@
+var config = {
+        apiKey: "AIzaSyDH0vwzO9Ob3GoeYU_-W_PfjOAtbv4V2R0",
+        authDomain: "misilearning.firebaseapp.com",
+        databaseURL: "https://misilearning.firebaseio.com",
+        projectId: "misilearning",
+        storageBucket: "misilearning.appspot.com",
+        messagingSenderId: "290441985709"
+      };
+      firebase.initializeApp(config);
